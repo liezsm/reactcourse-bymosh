7283d6b9f593aced7de478632cf59d681b6aa35a
liezsm <liez.dev@gmail.com>
2022-03-02 13:49:23 +0800

    Testing bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+
+import Counter from './components/Counter'
 
 function App(){
 
     return (
         <div>
             Hello world!
+
+            <Counter />
         </div>
     )
 }
